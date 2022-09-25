d9dfeba0c092ed26e1670bf7ed3b56ab5af84ed3
vanlakaf <56647233+vanlakaf@users.noreply.github.com>
2022-09-25 11:00:03 +0000

    Adding addActionListener method on Store.

--- index.d.ts
+++ index.d.ts
@@ -1,55 +1,85 @@
 type Actions<T> = {
-    [action: string]: (
-        context: {
-            commit: (commitName: string, commitPayload?: any) => void;
-            state?: T;
-        },
-        payload?: any
-    ) => Promise<any | void>;
+  [action: string]: (
+    context: {
+      commit: (commitName: string, commitPayload?: any) => void;
+      state?: T;
+    },
+    payload?: any
+  ) => Promise<any | void>;
+};
+
+type EventAction<T> = { (event: { state?: T }): void };
+
+type ActionListeners<T> = {
+  [key: keyof Actions<T>]: {
+    [member: string]: EventAction<T>;
+  };
 };
 
 type Mutations<T> = {
-    [mutation: string]: (state: T, payload?: any) => T;
+  [mutation: string]: (state: T, payload?: any) => T;
 };
 
 interface InitialConfig<T> {
-    state: T;
-    mutations: Mutations<T>;
-    actions: Actions<T>;
+  state: T;
+  mutations: Mutations<T>;
+  actions: Actions<T>;
 }
 
 interface ReactiveUnregistration {
-    unregister: () => void;
-    callHandler: () => void;
+  unregister: () => void;
+  callHandler: () => void;
 }
 
+type AddActionListener<T> = {
+  (
+    actionName: keyof ActionListeners<T>,
+    cb: EventAction<T>,
+    options?: { once: boolean }
+  ): {
+    addActionListener: AddActionListener;
+  };
+};
+
 export declare class Reactive<T> {
-    private initialValue: T;
-    private s: T;
-    private registeredActions: { [key: string]: (value: T) => void };
+  private initialValue: T;
+  private s: T;
+  private registeredActions: { [key: string]: (value: T) => void };
 
-    constructor(initialValue: T);
+  constructor(initialValue: T);
 
-    get state(): T;
+  get state(): T;
 
-    register(cb: (value: T) => void): ReactiveUnregistration;
+  register(cb: (value: T) => void): ReactiveUnregistration;
 
-    setState(value: T): void;
+  setState(value: T): void;
 
-    resetState(): void
+  resetState(): void;
 }
 
 export declare class Store<T> {
-    private s: Reactive<T>;
-    private mutations: Mutations<T>;
-    private actions: Actions<T>;
+  private s: Reactive<T>;
+  private mutations: Mutations<T>;
+  private actions: Actions<T>;
+  private actionListeners: ActionListeners<T>;
 
-    constructor(config: InitialConfig<T>);
+  constructor(config: InitialConfig<T>);
 
-    get state(): Reactive<T>;
-    
-    private commit(commitName: keyof Mutations<T>, commitPayload?: any): Promise<void>;
+  get state(): Reactive<T>;
 
-    dispatch(actionName: keyof Actions<T>, payload?: any): Promise<any>;
+  get snapshot(): T;
+
+  private commit(
+    commitName: keyof Mutations<T>,
+    commitPayload?: any
+  ): Promise<void>;
+
+  private runActionListeners(actionName: keyof Actions<T>): void;
+
+  dispatch(actionName: keyof Actions<T>, payload?: any): Promise<any>;
+
+  /**
+   * @method
+   */
+  addActionListener: AddActionListener<T>;
 }
-
--- index.js
+++ index.js
@@ -1,90 +1,151 @@
+/**
+ * @template T
+ * @param { T } obj
+ * @returns { T }
+ */
 function clone(obj) {
-    return JSON.parse(JSON.stringify({ obj })).obj;
+  return JSON.parse(JSON.stringify({ obj })).obj;
 }
 
 function Reactive(initialValue) {
-    let s = clone(initialValue);
-    let registeredActions = {};
+  let s = clone(initialValue);
+  let registeredActions = {};
 
-    Object.defineProperty(this, "state", {
-        get() {
-            return s;
-        },
-        set(value){
-            console.log("Cannot set state. Instead, use setState method.");
-        },
-    });
+  Object.defineProperty(this, "state", {
+    get() {
+      return s;
+    },
+    set(value) {
+      console.log("Cannot set state. Instead, use setState method.");
+    },
+  });
 
-    this.register = function(cb) {
-        if (typeof cb !== "function") {
-            throw Error("Listen callback must be a function.");
+  this.register = function (cb) {
+    if (typeof cb !== "function") {
+      throw Error("Listen callback must be a function.");
+    }
+
+    const key = Object.keys(registeredActions).length;
+    registeredActions["_" + key] = cb;
+
+    return {
+      unregister: () => {
+        registeredActions["_" + key] = null;
+        cb = null;
+      },
+      callHandler: () =>
+        !cb || !registeredActions["_" + key]
+          ? () => {}
+          : registeredActions["_" + key](s),
+    };
+  };
+
+  runListeners = function () {
+    if (Object.keys(registeredActions).length !== 0) {
+      for (const key in registeredActions) {
+        if (Object.prototype.hasOwnProperty.call(registeredActions, key)) {
+          if (typeof registeredActions[key] === "function") {
+            registeredActions[key](s);
+          }
         }
+      }
+    }
+  };
 
-        const key = Object.keys(registeredActions).length;
-        registeredActions["_" + key] = cb;
+  this.setState = function (value) {
+    s = value;
+    runListeners();
+  };
 
-        return {
-            unregister: () => {
-                registeredActions['_' + key] = null;
-                cb = null;
-            },
-            callHandler: () => !cb ? () => {} : cb(s),
-        };
-    };
-
-    runListeners = function() {
-        if (Object.keys(registeredActions).length !== 0) {
-            for (const key in registeredActions) {
-                if (Object.prototype.hasOwnProperty.call(registeredActions, key)) {
-                    if (typeof registeredActions[key] === "function") {
-                        registeredActions[key](s);
-                    }
-                }
-            }
-        }
-    };
-
-    this.setState = function(value) {
-        s = value;
-        runListeners();
-    };
-
-    this.resetState = function() {
-        s = clone(initialValue);
-        runListeners();
-    };
+  this.resetState = function () {
+    s = clone(initialValue);
+    runListeners();
+  };
 }
 
 function Store(config) {
-    let s = new Reactive(config.state);
-    let mutations = config.mutations;
-    let actions = config.actions;
+  let s = new Reactive(config.state);
+  let mutations = config.mutations;
+  let actions = config.actions;
+  let actionListeners = {};
 
-    Object.defineProperty(this, "state", {
-        get() {
-            return s;
-        },
-        set(value) {
-            console.log("Cannot set state. Instead, create action in store configuration, and dispatch it with the data you would update the state of store.");
-        },
+  for (let key in actions) {
+    actionListeners[key] = {};
+  }
+
+  Object.defineProperty(this, "state", {
+    get() {
+      return s;
+    },
+    set(value) {
+      console.log(
+        "Cannot set state. Instead, create action in store configuration, and dispatch it with the data you would update the state of store."
+      );
+    },
+  });
+
+  Object.defineProperty(this, "snapshot", {
+    get() {
+      return s.state;
+    },
+    set(value) {
+      console.log(
+        "Cannot set state. Instead, create action in store configuration, and dispatch it with the data you would update the state of store."
+      );
+    },
+  });
+
+  const runActionListeners = function (actionName) {
+    const listeners = actionListeners[actionName];
+    if (!listeners) {
+      return;
+    }
+    const keys = Object.keys(listeners);
+    keys.forEach((key) => {
+      if (!listeners[key]) {
+        // console.log(`Action listener ${key} not found or null.`);
+        return;
+      }
+      listeners[key]({ state: clone(s.state) });
     });
+  };
 
-    commit = function(commitName, commitPayload) {
-        if (!mutations[commitName]) {
-            throw Error("Cannot find mutation " + commitName);
-        }
-        return new Promise((resolve, reject) => {
-            s.setState(mutations[commitName](s.state, commitPayload));
-            resolve();
-        });
+  const commit = function (commitName, commitPayload) {
+    if (!mutations[commitName]) {
+      throw Error("Cannot find mutation " + commitName);
+    }
+    return new Promise((resolve, reject) => {
+      s.setState(mutations[commitName](s.state, commitPayload));
+      resolve();
+    });
+  };
+
+  this.dispatch = function (actionName, payload) {
+    if (!actions[actionName]) {
+      throw Error("Cannot dispatch action " + actionName);
+    }
+    const p = actions[actionName]({ commit, state: clone(s.state) }, payload);
+    runActionListeners(actionName);
+    return p;
+  };
+
+  this.addActionListener = function (actionName, cb, options = undefined) {
+    const listeners = actionListeners[actionName];
+    if (!listeners) {
+      throw Error("Cannot listen action " + actionName);
     }
 
-    this.dispatch = function(actionName, payload) {
-        if (!actions[actionName]) {
-            throw Error("Cannot dispatch action " + actionName);
-        }
-        return actions[actionName]({ commit, state: s }, payload);
-    }
+    const keysLength = Object.keys(listeners).length;
+    actionListeners[actionName]["_" + keysLength] = (...args) => {
+      cb(...args);
+      const { once = false } = options || {};
+      if (once) {
+        actionListeners[actionName]["_" + keysLength] = null;
+      }
+    };
+    
+    return { addActionListener: this.addActionListener };
+  };
 }
 
 exports.Reactive = Reactive;
